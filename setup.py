a0662ad038fae5399270e11c6f88d6afab369d7e
Edward Kim <eddotman@gmail.com>
2015-02-05 13:28:05 -0500

    added scaffolding

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+name = 'synthesisapi',
+packages = ['synthesisapi'], # this must be the same as the name above
+version = '1.0',
+description = 'Predictive materials synthesis database - API',
+author = 'Edward Kim',
+author_email = 'eddotman@gmail.com',
+url = 'https://github.com/eddotman/synthesis-api', # use the URL to the github repo
+download_url = 'https://www.github.com/eddotman/synthesis-api/tarball/1.0',
+keywords = ['materials', 'synthesis', 'database', 'api'], # arbitrary keywords
+)
