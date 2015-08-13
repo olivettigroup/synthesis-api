675b54f9b77d95e1bebba599f746e0345bcf4c71
Edward Kim <eddotman@gmail.com>
2015-08-13 15:04:37 -0400

    better port fix

--- synthesisapi/run.py
+++ synthesisapi/run.py
@@ -1,8 +1,10 @@
 from eve import Eve
+from os import (environ)
+
 e = Eve()
 
 def app():
-  e.run()
+  e.run(port=int(environ.get('PORT', 5000)))
 
 if __name__ == '__main__':
   app()
--- synthesisapi/settings.py
+++ synthesisapi/settings.py
@@ -2,7 +2,6 @@ from schemas import (target_entity)
 from os import (environ)
 
 MONGO_URI = environ.get('MONGOLAB_URI')
-MONGO_PORT = int(environ.get('PORT', 5000))
 
 PUBLIC_METHODS = ['GET']
 PUBLIC_ITEM_METHODS = ['GET']
