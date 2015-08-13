48b30e55bd6ab8f963dc0da035d1f3381dcad7a9
Edward Kim <eddotman@gmail.com>
2015-08-13 14:55:34 -0400

    updated port

--- synthesisapi/settings.py
+++ synthesisapi/settings.py
@@ -2,6 +2,7 @@ from schemas import (target_entity)
 from os import (environ)
 
 MONGO_URI = environ.get('MONGOLAB_URI')
+MONGO_PORT = int(environ.get('PORT', 5000))
 
 PUBLIC_METHODS = ['GET']
 PUBLIC_ITEM_METHODS = ['GET']
