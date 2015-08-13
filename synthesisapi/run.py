810d2e8e5650a4d6f0ff24f85c52b10138d58462
Edward Kim <eddotman@gmail.com>
2015-08-13 15:12:04 -0400

    changes to run.py

--- synthesisapi/run.py
+++ synthesisapi/run.py
@@ -1,10 +1,15 @@
 from eve import Eve
 from os import (environ)
 
-e = Eve()
+# Heroku support: bind to PORT if defined, otherwise default to 5000.
+if 'PORT' in environ:
+    port = int(environ.get('PORT'))
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
 
-def app():
-  e.run(port=int(environ.get('PORT', 5000)))
+app = Eve()
 
 if __name__ == '__main__':
-  app()
+    app.run(host=host, port=port)
