71d4d4f2c4d34a5c44ee42b71df5841e28c0fd5a
Edward Kim <eddotman@gmail.com>
2015-08-14 13:33:12 -0400

    fixed route types

--- synthesisapi/settings.py
+++ synthesisapi/settings.py
@@ -6,8 +6,8 @@ MONGO_URI = environ.get('MONGOLAB_URI')
 PUBLIC_METHODS = ['GET', 'POST']
 PUBLIC_ITEM_METHODS = ['GET', 'POST']
 
-RESOURCE_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
-ITEM_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PUT', 'DELETE']
 
 DOMAIN = {
   'materials': target_entity
