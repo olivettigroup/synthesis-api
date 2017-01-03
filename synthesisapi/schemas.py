22e7816a597f0fd93800c7bc7c62c1e755b20aa7
Edward Kim <eddotman@gmail.com>
2017-01-03 17:55:36 -0500

    update schema

--- synthesisapi/schemas.py
+++ synthesisapi/schemas.py
@@ -1,18 +1,17 @@
 material = {
   'item_title': 'material',
   'schema': {
-    'sample_dois': {
+    'dois': {
       'type': 'list'
     },
-
     'associated_materials': {
       'type': 'list'
     },
     'temperature_histogram': {
       'type': 'dict',
       'schema': {
-        'x': {'type': 'list'},
-        'y': {'type': 'list'}
+        'temperatures': {'type': 'list'},
+        'frequencies': {'type': 'list'}
       }
     },
     'synthesis_descriptions': {
